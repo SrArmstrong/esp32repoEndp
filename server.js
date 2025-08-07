10a82287ca32a9535fa12ea1baa8ea48bc1f2ebf
SrArmstrong <cetisaldavalde@gmail.com>
2025-08-06 21:01:01 -0600

    Modificado para .bin

--- server.js
+++ server.js
@@ -2,24 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!, GET');
-    console.log('Respuesta esperada: ', JSON.stringify({ message: 'ESP32 - Funcionando y conectado!', nombre: 'Sergio' }));
-});
-
-app.post('/post', (req, res) => {
-    req.on('data', (data) => {
-        console.log('Datos recibidos: ', data.toString());
-    });
-    res.send('Recibido, POST');
-});
-
-app.put('/put', (req, res) => {
-    req.on('data', (data) => {
-        console.log('Datos actualizados: ', data.toString());
-    });
-    res.send('Datos actualizados, PUT');
-});
+// Carpeta pública donde está blink.bin
+app.use('/firmware', express.static('public/firmware'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
