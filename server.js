8106d521c9c123c7028c4016183d3ba530f94663
SrArmstrong <cetisaldavalde@gmail.com>
2025-08-08 18:51:01 -0600

    Producción de .bin

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/firmware/version', (req, res) => {
+    res.json({
+        version: "1.0",
+        url: "http://192.168.1.75:3000/firmware/blink.bin",
+        checksum: "2a5d4c...",
+        mandatory: false
+    });
+});
+
 // Carpeta pública donde está blink.bin
 app.use('/firmware', express.static('public/firmware'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
